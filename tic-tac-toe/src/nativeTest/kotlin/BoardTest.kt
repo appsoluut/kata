190a4265df4c03c3e08214c12db6dec34cbad6f4
Paul Hameteman <paul.hameteman@ns.nl>
2025-03-12 08:06:40 +0100

    Make proper test cases

--- tic-tac-toe/src/nativeTest/kotlin/BoardTest.kt
+++ tic-tac-toe/src/nativeTest/kotlin/BoardTest.kt
@@ -1,5 +1,6 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class BoardTest {
@@ -7,12 +8,61 @@ class BoardTest {
     private val sut = Board()
 
     @Test
-    fun validSolutionTest() {
-        assertTrue(true, "Hello")
+    fun winnerBeforeTheBoardIsFullTest() {
+        // X X O
+        // X   O
+        //     O
+        sut.set(Player.X, 0, 0)
+        sut.set(Player.X, 0, 1)
+        sut.set(Player.X, 1, 0)
+        sut.set(Player.O, 2, 0)
+        sut.set(Player.O, 2, 1)
+        sut.set(Player.O, 2, 2)
+        assertFalse(sut.isFull(), "Board should not be full yet")
+        assertEquals(Player.O, sut.hasWinner(), "Player.O should have won")
+    }
+
+    @Test
+    fun noWinnerTest() {
+        // Board with just one move
+        sut.set(Player.X, 0, 0)
+        assertFalse(sut.isFull(), "Board should not be full yet")
+        assertEquals(null, sut.hasWinner(), "No player should have won yet")
+    }
+
+    @Test
+    fun fullBoardWithWinnerTest() {
+        // X O O
+        // O X O
+        // X O X
+        sut.set(Player.X, 0, 0)
+        sut.set(Player.O, 1, 0)
+        sut.set(Player.O, 2, 0)
+        sut.set(Player.O, 0, 1)
+        sut.set(Player.X, 1, 1)
+        sut.set(Player.O, 2, 1)
+        sut.set(Player.X, 0, 2)
+        sut.set(Player.O, 1, 2)
+        sut.set(Player.X, 2, 2)
+        assertTrue(sut.isFull(), "Board should be full")
+        assertEquals(Player.X, sut.hasWinner(), "Player.X should have won")
     }
 
     @Test
     fun fullBoardNoWinnerTest() {
+        // X O O
+        // O X X
+        // X O O
+        sut.set(Player.X, 0, 0)
+        sut.set(Player.O, 1, 0)
+        sut.set(Player.O, 2, 0)
+        sut.set(Player.O, 0, 1)
+        sut.set(Player.X, 1, 1)
+        sut.set(Player.X, 2, 1)
+        sut.set(Player.X, 0, 2)
+        sut.set(Player.O, 1, 2)
+        sut.set(Player.O, 2, 2)
         assertTrue(sut.isFull(), "Board should be full")
+        assertEquals(null, sut.hasWinner(), "No player should have won")
     }
 }
